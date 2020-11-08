1abf9964a1a113fd8627bfc9d56cb706a97529bc
atulcodex <atulkprajapati2000@gmail.com>
2020-11-08 19:59:43 +0530

    working contact form ready

---
+++ contactengine.php
@@ -0,0 +1,39 @@
+<?php
+
+$EmailFrom = "abdulppj@gmail.com";
+$EmailTo = "atulkprajapati2000@gmail.com";
+$Subject = $_POST['Name']." Contacted you from your portfolio site";
+$Name = Trim(stripslashes($_POST['Name']));
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Message = Trim(stripslashes($_POST['Message'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=index.html\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
